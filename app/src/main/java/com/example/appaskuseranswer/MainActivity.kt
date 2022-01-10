d4112fa510878113efb1b577e86c009d3f7b0137
harangAi <harang_ai@naver.com>
2022-01-10 19:41:01 +0900

    최초 커밋

---
+++ app/src/main/java/com/example/appaskuseranswer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appaskuseranswer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
