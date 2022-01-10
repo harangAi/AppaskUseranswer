633df7ffb5d7714f4477e013cb1ffb653a21dc94
harangAi <harang_ai@naver.com>
2022-01-10 19:41:01 +0900

    "하단 색상 변경 완료"

--- app/src/main/java/com/example/appaskuseranswer/MainActivity.kt
+++ app/src/main/java/com/example/appaskuseranswer/MainActivity.kt
@@ -2,11 +2,54 @@ package com.example.appaskuseranswer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.appaskuseranswer.navigation.DetailViewFragment
+import com.example.appaskuseranswer.navigation.ListFragment
+import com.example.appaskuseranswer.navigation.QuestionFragment
+import com.example.appaskuseranswer.navigation.SettingFragment
+import com.example.appaskuseranswer.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener{
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+         when (p0.itemId) {
+             R.id.page_question -> {
+                 var questionFragment = QuestionFragment()
+                 supportFragmentManager.beginTransaction()
+                     .replace(R.id.container, questionFragment).commit()
+                 return true
+             }
+
+             R.id.page_list -> {
+                 var listFragment = ListFragment()
+                 supportFragmentManager.beginTransaction()
+                     .replace(R.id.container, listFragment).commit()
+                 return true
+             }
+
+             R.id.page_setting -> {
+                 var settingFragment = SettingFragment()
+                 supportFragmentManager.beginTransaction()
+                     .replace(R.id.container, settingFragment).commit()
+                 return true
+             }
+         }
+         var detailViewFragment = DetailViewFragment()
+         supportFragmentManager.beginTransaction()
+             .replace(R.id.container, detailViewFragment).commit()
+         return false
+     }
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+         super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_main)
+         bottom_navi.setOnNavigationItemSelectedListener(this)
+
+     }
+
+
+
+}//
---
+++ app/src/main/java/com/example/appaskuseranswer/navigation/QuestionFragment.kt
@@ -0,0 +1,20 @@
+package com.example.appaskuseranswer.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.appaskuseranswer.R
+
+class QuestionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_question, container, false)
+        return view
+    }
+}
