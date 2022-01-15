328bef9fd43213701f87c6b80f47b55c60e01143
abc20210708 <87113813+abc20210708@users.noreply.github.com>
2022-01-15 18:43:36 +0900

    AlertDialog 사용해보기

--- app/src/main/java/com/example/appaskuseranswer/navigation/SettingFragment.kt
+++ app/src/main/java/com/example/appaskuseranswer/navigation/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.appaskuseranswer.navigation
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,9 @@ class SettingFragment : Fragment() {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_setting, container, false)
         return view
     }
+
+
+
 }
 
 
