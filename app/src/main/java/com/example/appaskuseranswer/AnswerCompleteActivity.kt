ecd1659a5cb084e8d7e9d1d186aca0aed12c123b
hhsssu <87113802+hhsssu@users.noreply.github.com>
2022-01-17 02:42:54 +0900

    [22.01.16] 질문 답변 완료 레이아웃 제작
    답변 완료 후 질문 탭 선택 시 뜨게 하는 레이아웃 제작.

---
+++ app/src/main/java/com/example/appaskuseranswer/AnswerCompleteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appaskuseranswer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer_complete)
+    }
+}
