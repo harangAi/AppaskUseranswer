fe19a553a2706c60ebd8b20cf274a112faa5c358
hhsssu <87113802+hhsssu@users.noreply.github.com>
2022-01-17 02:39:33 +0900

    [22.01.16] 답변 작성 레이아웃 제작
    답변 작성하는 레이아웃 디자인 제작.
    질문 레이아웃 일부 수정(로고 이미지, 날짜 텍스트 색상 변경)

---
+++ app/src/main/java/com/example/appaskuseranswer/AnswerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appaskuseranswer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
