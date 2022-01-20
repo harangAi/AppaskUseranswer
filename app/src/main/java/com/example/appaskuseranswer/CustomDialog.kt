7d17b02ee160decf7d17b85e6d1ab6ef8a787ab6
abc20210708 <87113813+abc20210708@users.noreply.github.com>
2022-01-20 17:44:13 +0900

    대화상자 띄우기

---
+++ app/src/main/java/com/example/appaskuseranswer/CustomDialog.kt
@@ -0,0 +1,38 @@
+package com.example.appaskuseranswer
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.example.appaskuseranswer.databinding.ActivityMainBinding
+
+class CustomDialog: DialogFragment() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ActivityMainBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+}//
--- app/src/main/java/com/example/appaskuseranswer/navigation/SettingFragment.kt
+++ app/src/main/java/com/example/appaskuseranswer/navigation/SettingFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.appaskuseranswer.CustomDialog
 import com.example.appaskuseranswer.R
 import com.example.appaskuseranswer.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_setting.*
+import kotlinx.android.synthetic.main.setting_notice_dialog.*
 
 
-class SettingFragment : Fragment() {
+class SettingFragment : DialogFragment() {
 
+    private val dialog = CustomDialog()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,14 +27,26 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_setting, container, false)
         return view
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_setting_notice).setOnClickListener {
+            val dialog = CustomDialog()
+            dialog.show(
+                childFragmentManager, "custom"
+            )
+        }
     }
 
 
-
-        }
+        }//
 
 
 
