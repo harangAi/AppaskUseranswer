f3f4872a708bca24902a6b466c9948f06904340a
abc20210708 <87113813+abc20210708@users.noreply.github.com>
2022-01-21 17:41:49 +0900

    MainActivity 수정 완료

--- app/src/main/java/com/example/appaskuseranswer/MainActivity.kt
+++ app/src/main/java/com/example/appaskuseranswer/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appaskuseranswer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.example.appaskuseranswer.navigation.ListFragment
 import com.example.appaskuseranswer.navigation.QuestionFragment
 import com.example.appaskuseranswer.navigation.SettingFragment
@@ -10,45 +11,66 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() , BottomNavigationView.OnNavigationItemSelectedListener{
+class MainActivity : AppCompatActivity() {
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-         when (p0.itemId) {
-             R.id.page_question -> {
-                 var questionFragment = QuestionFragment()
-                 supportFragmentManager.beginTransaction()
-                     .replace(R.id.container, questionFragment).commit()
-                 return true
-             }
+    private lateinit var settingFragment: SettingFragment
 
-             R.id.page_list -> {
-                 var listFragment = ListFragment()
-                 supportFragmentManager.beginTransaction()
-                     .replace(R.id.container, listFragment).commit()
-                 return true
-             }
 
-             R.id.page_setting -> {
-                 var settingFragment = SettingFragment()
-                 supportFragmentManager.beginTransaction()
-                     .replace(R.id.container, settingFragment).commit()
-                 return true
-             }
-         }
-        /*setContentView(R.layout.activity_main)
-        bottom_navi.setOnNavigationItemSelectedListener(this)*/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initFragment()
+
+        var bnv_main = findViewById(R.id.bottom_navi) as BottomNavigationView
+
+        bnv_main.run { setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.page_question -> {
+                    var questionFragment = QuestionFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, questionFragment).commit()
+
+                }
+
+                R.id.page_list -> {
+                    var listFragment = ListFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, listFragment).commit()
+
+                }
+
+                R.id.page_setting -> {
+                    var settingFragment = SettingFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, settingFragment).commit()
+
+                }
+
+            }
+            true
+
+        }
+
+        selectedItemId = R.id.page_question
+
+        }
+
+
+    }
+
+
+
+
+    private fun initFragment() {
+        val questionFragment = QuestionFragment()
+        commitFragment(questionFragment)
+    }
+
+    private fun commitFragment(fragment: Fragment) {
         var questionFragment = QuestionFragment()
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, questionFragment).commit()
-         return false
-     }
+    }
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_main)
-         bottom_navi.setOnNavigationItemSelectedListener(this)
-
-
-
-         }
-}//
+}////
