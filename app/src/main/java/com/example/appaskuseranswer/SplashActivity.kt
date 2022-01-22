a7f68caa1ff481af415e2366972bda799f39fcba
hhsssu <87113802+hhsssu@users.noreply.github.com>
2022-01-23 02:41:50 +0900

    [22.01.22] gradle 버전 수정 및 뷰바인딩 선언
    gradle 7.0.4 -> 3.6.2로 변경
    build.grade(Module)에 뷰바인딩 선언

--- app/src/main/java/com/example/appaskuseranswer/SplashActivity.kt
+++ app/src/main/java/com/example/appaskuseranswer/SplashActivity.kt
@@ -1,10 +1,10 @@
 package com.example.appaskuseranswer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 
 class SplashActivity : AppCompatActivity() {
