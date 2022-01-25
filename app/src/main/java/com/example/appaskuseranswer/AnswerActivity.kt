ff8825022819f2d09f8699e9a6c47bc4ac8ff3cb
hhsssu <87113802+hhsssu@users.noreply.github.com>
2022-01-26 01:47:59 +0900

    [22.01.25] 백업용 커밋 (이벤트 작성 중)

--- app/src/main/java/com/example/appaskuseranswer/AnswerActivity.kt
+++ app/src/main/java/com/example/appaskuseranswer/AnswerActivity.kt
@@ -1,11 +1,45 @@
 package com.example.appaskuseranswer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 
 class AnswerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
+    }
+
+    val eventHandler = object : DialogInterface.OnClickListener {
+        override fun onClick(p0: DialogInterface?, p1: Int) {
+            when {
+                p1 == DialogInterface.BUTTON_POSITIVE -> {
+                    Log.d("yes", "positive button click")
+                    finish() //앱종료가 됨 (여기에선 앱종료가 아닌 이전 페이지(QuestionFragment)로 이동하는 것으로 수정할 것.)
+                }
+                p1 == DialogInterface.BUTTON_NEGATIVE -> {
+                    Log.d("no", "negative button click")
+                    null
+                }
+            }
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        when (keyCode) {
+            KeyEvent.KEYCODE_BACK -> {
+                Log.d("back", "뒤로가기 버튼 누름")
+                AlertDialog.Builder(this).run {
+                    setMessage("글 작성을 취소하시나요?")
+                    setPositiveButton("예", eventHandler)
+                    setNegativeButton("아니오", eventHandler)
+                    show()
+                }
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
--- app/src/main/java/com/example/appaskuseranswer/navigation/QuestionFragment.kt
+++ app/src/main/java/com/example/appaskuseranswer/navigation/QuestionFragment.kt
@@ -1,20 +1,52 @@
 package com.example.appaskuseranswer.navigation
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.appaskuseranswer.AnswerActivity
 import com.example.appaskuseranswer.R
+import com.example.appaskuseranswer.databinding.FragmentQuestionBinding
 
 class QuestionFragment : Fragment() {
+    lateinit var binding: FragmentQuestionBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_question, container, false)
+        //바인딩 객체 획득
+        binding = FragmentQuestionBinding.inflate(inflater, container, false)
+        return binding.root
+
+        var view =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_question, container, false)
         return view
+
+        //알림창
+        var quesAlert = activity?.let {
+            AlertDialog.Builder(context).run {
+                setTitle("1번 질문")
+                setMessage("질문에 답변을 남기시겠어요? (답변은 하루에 하나의 질문만 가능합니다!)")
+                setPositiveButton("작성", null)
+                setNegativeButton("취소", null)
+                show()
+            }
+        }
+
+        //1번 질문 클릭 이벤트(미완성)
+        binding.question1.setOnClickListener {
+            quesAlert
+
+            activity?.let {
+                val intent = Intent(context, AnswerActivity::class.java)
+            }
+        }
     }
 }
+
+
